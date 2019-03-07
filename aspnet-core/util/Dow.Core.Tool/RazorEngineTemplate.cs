490607aae708f4edbc544fc7a2ab540be27dfd07
hongyixiangshuai <30709104+hongyixiangshuai@users.noreply.github.com>
2019-03-07 20:52:17 +0800

    添加工具类

---
+++ aspnet-core/util/Dow.Core.Tool/RazorEngineTemplate.cs
@@ -0,0 +1,14 @@
+﻿using RazorEngine;
+using RazorEngine.Templating;
+using System;
+
+namespace Dow.Core.Tool
+{
+    public class RazorEngineTemplate
+    {
+        public static string  GetValue(string templateSource, string key, Type modelType = null, object model = null)
+        {
+            return Engine.Razor.RunCompile(templateSource, key, modelType, model);
+        }
+    }
+}
